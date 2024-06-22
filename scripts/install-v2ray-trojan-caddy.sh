31b99eb3b1dd9f287c7d4df06d4093906f9e095b
allenlo-dev <53904654+allenlo-dev@users.noreply.github.com>
2024-06-22 21:45:40 +0800

    Add files via upload

--- scripts/install-v2ray-trojan-caddy.sh
+++ scripts/install-v2ray-trojan-caddy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 domain=""
-vless_id=""
-vless_path=""
+trojan_passwd=""
+trojan_path=""
+vmess_id=""
+vmess_path=""
 
 root_dir=""
 function judgment_param() {
@@ -14,28 +16,44 @@ function judgment_param() {
     fi
 
     if [ ! -n "$2" ]; then
-        vless_id='26E3EA30-57B3-95C7-3424-2FD8EEFB814C'
+        trojan_passwd='M&~buTYoeSavlu-C0PRaGTsR'
     else
-        vless_id=$2
+        trojan_passwd=$2
     fi
 
     if [ ! -n "$3" ]; then
-        vless_path='/less'
+        trojan_path='/troa'
     else
         if [[ $3 =~ ^/.* ]]; then
-            vless_path=$3
+            trojan_path=$3
         else
-            vless_path="/$3"
+            trojan_path="/$3"
         fi
     fi
 
     if [ ! -n "$4" ]; then
+        vmess_id='26E3EA30-57B3-95C7-3424-2FD8EEFB814C'
+    else
+        vmess_id=$4
+    fi
+
+    if [ ! -n "$5" ]; then
+        vmess_path='/mess'
+    else
+        if [[ $5 =~ ^/.* ]]; then
+            vmess_path=$5
+        else
+            vmess_path="/$5"
+        fi
+    fi
+
+    if [ ! -n "$6" ]; then
         root_dir='/home/wwwroot'
     else
-        if [[ $4 =~ ^/.* ]]; then
-            root_dir=$4
+        if [[ $6 =~ ^/.* ]]; then
+            root_dir=$6
         else
-            root_dir="/$4"
+            root_dir="/$6"
         fi
     fi
 }
@@ -127,7 +145,6 @@ function install_v2ray() {
     fi
 
     #bash <(curl -L -s https://install.direct/go.sh)
-    #bash <(curl -L -s https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh) --version v4.45.2
     bash <(curl -L -s https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
     systemctl enable v2ray
     #systemctl start v2ray
@@ -139,44 +156,73 @@ function install_v2ray() {
         json_path='/etc/v2ray/config.json'
     fi
 
-cat << EOF > ${json_path}
+cat << EOF > $json_path
 {
-  "inbounds": [
-    {
-      "port": 1521,
-      "listen":"127.0.0.1",
-      "protocol": "vless",
-      "settings": {
-        "decryption": "none",
-        "clients": [
-          {
-            "id": "${vless_id}",
-            "level": 1,
-            "alterId": 0
-          }
-        ]
-      },
-      "streamSettings": {
-        "network": "ws",
-        "wsSettings": {
-          "path": "${vless_path}"
+    "inbounds": [
+        {
+            "port": 1521,
+            "listen": "127.0.0.1",
+            "protocol": "trojan",
+            "settings": {
+                "decryption": "none",
+                "clients": [
+                    {
+                        "email": "atro@${domain}",
+                        "password": "${trojan_passwd}"
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "ws",
+                "security": "none",
+                "wsSettings": {
+                    "path": "${trojan_path}"
+                }
+            },
+            "sniffing": {
+                "enabled": true,
+                "destOverride": [
+                    "http",
+                    "tls"
+                ]
+            }
         }
-      }
-    }
-  ],
-  "outbounds": [
-    {
-      "protocol": "freedom",
-      "settings": {}
-    }
-  ]
+    ],
+    "inboundDetour": [
+        {
+            "port": 681,
+            "listen": "127.0.0.1",
+            "protocol": "vmess",
+            "settings": {
+                "clients": [
+                    {
+                        "id": "${vmess_id}",
+                        "level": 1,
+                        "alterId": 0
+                    }
+                ]
+            },
+            "streamSettings": {
+                "network": "ws",
+                "wsSettings": {
+                    "path": "${vmess_path}"
+                }
+            }
+        }
+    ],
+    "outbounds": [
+        {
+            "protocol": "freedom",
+            "settings": {}
+        }
+    ]
 }
 EOF
 }
 
 #install caddy
 caddy_version=1
-function find_proxy_insert_line() {
+function find_proxy_insert_line_1() {
     line_num=$(sed -n -e "/${domain} {/=" /etc/caddy/Caddyfile)
     end_num=$(sed -n '$=' /etc/caddy/Caddyfile)
 
@@ -209,9 +255,56 @@ function find_proxy_insert_line() {
     return ${line_num}
 }
 
+function find_proxy_insert_line_2() {
+    line_num=$(sed -n -e "/${domain} {/=" /etc/caddy/Caddyfile)
+    end_num=$(sed -n '$=' /etc/caddy/Caddyfile)
+
+    find_result=0
+    while [ ${line_num} -le ${end_num} ]; do
+        let line_num++
+        proxy_str=$(sed -n "${line_num}p" /etc/caddy/Caddyfile)
+        if [ "${proxy_str}" == "}" ]; then
+            break
+        fi
+
+        if [[ ${proxy_str} =~ "127.0.0.1:681" ]]; then
+            find_result=1
+            break
+        fi
+    done
+
+    if [ ${find_result} -eq 0 ]; then
+        line_num=$(sed -n -e "/allen@${domain}/=" /etc/caddy/Caddyfile)
+    else
+        sed -i "${line_num} d" /etc/caddy/Caddyfile
+        if [ ${caddy_version} -eq 1 ]; then
+            sed -i "${line_num} d" /etc/caddy/Caddyfile
+            sed -i "${line_num} d" /etc/caddy/Caddyfile
+            sed -i "${line_num} d" /etc/caddy/Caddyfile
+        fi
+        let line_num--
+    fi
+
+    return ${line_num}
+}
+
 function insert_proxy_to_caddy_v1() {
     caddy_version=1
-    find_proxy_insert_line
+    find_proxy_insert_line_2
+    line_num=$?
+    sed -i "${line_num} a\  proxy ${vmess_path} 127.0.0.1:681 {" /etc/caddy/Caddyfile
+    let line_num++
+
+    sed -i "${line_num} a\    websocket" /etc/caddy/Caddyfile
+    let line_num++
+
+    sed -i "${line_num} a\    header_upstream -Origin" /etc/caddy/Caddyfile
+    let line_num++
+
+    sed -i "${line_num} a\  }" /etc/caddy/Caddyfile
+    let line_num++
+
+    find_proxy_insert_line_1
     line_num=$?
     sed -i "${line_num} a\  proxy ${vless_path} 127.0.0.1:1521 {" /etc/caddy/Caddyfile
     let line_num++
@@ -226,9 +319,13 @@ function insert_proxy_to_caddy_v1() {
     let line_num++
 }
 
-function insert_proxy_to_caddy_v2() {
+function insert_proxy_to_caddy_v2() { 
     caddy_version=2
-    find_proxy_insert_line
+    find_proxy_insert_line_2
+    line_num=$?
+    sed -i "${line_num} a\  reverse_proxy ${vmess_path} 127.0.0.1:681" /etc/caddy/Caddyfile
+
+    find_proxy_insert_line_1
     line_num=$?
     sed -i "${line_num} a\  reverse_proxy ${vless_path} 127.0.0.1:1521" /etc/caddy/Caddyfile
 }
@@ -240,6 +337,10 @@ ${domain} {
   root ${root_dir}/${domain}
   tls allen@${domain}
   proxy ${vless_path} 127.0.0.1:1521 {
+    websocket
+    header_upstream -Origin
+  }
+  proxy ${vmess_path} 127.0.0.1:681 {
     websocket
     header_upstream -Origin
   }
@@ -257,6 +358,7 @@ ${domain} {
   root * ${root_dir}/${domain}
   tls allen@${domain}
   reverse_proxy ${vless_path} 127.0.0.1:1521
+  reverse_proxy ${vmess_path} 127.0.0.1:681
 }
 EOF
 }
@@ -284,10 +386,11 @@ function install_caddy_firewall() {
             append_domain_to_caddy_v2
         fi
     fi
-    
+
     echo ""
     echo -e "\033[36m------------- Caddy Config ---------------\033[0m"
     cat /etc/caddy/Caddyfile
+    echo ""
     echo -e "\033[36m------- V2RAY Host Settings(VLESS) -------\033[0m"
     echo "Address:    ${domain}"
     echo "Port:       443"
@@ -299,6 +402,19 @@ function install_caddy_firewall() {
     echo "Type:       none"
     echo "Host:       ${domain}"
     echo "Path        ${vless_path}"
+    echo "TLS: tls    AllowInsecure: false"
+    echo ""
+    echo -e "\033[36m------- V2RAY Host Settings(VMESS) -------\033[0m"
+    echo "Address:    ${domain}"
+    echo "Port:       443"
+    echo "ID:         ${vmess_id}"
+    echo "FLow:       xtls-rprx-origin"
+    echo "Encryption: none"
+    echo -e "\033[36m-------- V2RAY Transport Settings --------\033[0m"
+    echo "Network:    ws"
+    echo "Type:       none"
+    echo "Host:       ${domain}"
+    echo "Path        ${vmess_path}"
     echo "TLS: tls    AllowInsecure: false"
     echo ""
 
